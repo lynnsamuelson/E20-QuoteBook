58c144160ac603365c5857bce4fafd9bc14e0ab9
Lynn Samuelson <lynn.samuelson@gmail.com>
2022-08-30 20:01:17 -0500

    added click event listener to add new quote

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import {getQuotes} from './quotes.js'
+import {getQuotes, addNewQuote} from './quotes.js'
 
 const authors = [
   {
@@ -61,10 +61,74 @@ const displayQuotes = () => {
 
 displayQuotes()
 
-const newFuntion = () => {
-  const quotes = getQuotes()
-}
 
-// for(const quote of quotes) {
-//   //quote === quotes[i] above
-// }
+// event listener for the submit button
+document.addEventListener("click", (e) => {
+  if(e.target.id == "submitNewQuote") {
+    //get values from text box
+    const quote = document.getElementById("quoteText").value
+    const authorText = document.getElementById("quoteAuthor").value
+
+    // This finds the author object that matches the input author text
+    const authorObj = authors.find(({ firstName }) => firstName === authorText);
+    
+    // Finding if author is alive
+    const alive = document.querySelector("input[name=alive]:checked")?.value;
+    
+    //Long version
+    // let isAlive
+    // if(alive === "true") {
+      //   isAlive = true
+      // } else {
+        //   isAlive = false
+        // }
+        
+    //med length version
+    // let isAlive = false
+    // if(alive === "true") {
+    //   isAlive = true
+    // }
+    
+    // shortest - ternary version
+    const isAlive = alive === "true" ? true : false
+    
+    console.log(isAlive)
+
+    
+    // create a newQuote object
+    const newQuote = {
+      date: "08/30/22",
+      authorId: authorObj.id,
+      quote: quote,
+      hashTags: [],
+      isAuthorAlive: isAlive 
+    }
+    console.log("newQuote", newQuote)
+    addNewQuote(newQuote)
+  }
+
+  // import and invoke a function from quotes.js
+
+})
+
+document.addEventListener("stateChanged", (e) => {
+  displayQuotes()
+})
+  
+// HTML ids that code connects to 
+// StateChanged Event listener (custom)
+// start with empty array and add a quote
+
+// Steps to take
+// event listener for the submit button
+  //get values from text box
+  // create a newQuote object
+  // import and invoke a function from quotes.js
+
+// Add newQuote to quotes array (export function)
+  // use a function with array method push
+
+// Display on DOM
+  // dispatch custom event 
+  // Add event listener for custom event 
+    // displayQuotes()
--- quotes.js
+++ quotes.js
@@ -1,46 +1,5 @@
 
-const quotes = [
-  {
-    id: 1,
-    date: "08/02/2022",
-    authorId: 4,
-    quote: "Come eat lunch. The dogs are home and will eat you.",
-    hashTags: ["AI", "Amazon", "Skynet"],
-    isAuthorAlive: false
-  },
-  {
-    id: 2,
-    date: "08/01/2022",
-    authorId: 3,
-    quote: "Here's some practice js",
-    hashTags: ["JavaScript", "Code Stars", "E20", "Banana"],
-    isAuthorAlive: true
-  },
-  {
-    id: 3,
-    date: "07/27/2022",
-    authorId: 1,
-    quote: "We won't hold your hands!",
-    hashTags: ["Blanched Almonds", "Important things", "REMEMBER", "mister", "Future Dr. Noh", "Zombie"],
-    isAuthorAlive: true
-  },
-  {
-    id: 5,
-    date: "08/02/2022",
-    authorId: 1,
-    quote: "I can do it!",
-    hashTags: ["Notion Queen", "Future Dr. Noh", "Zombie"],
-    isAuthorAlive: true
-  },
-  {
-    id: 4,
-    date: "04/27/2022",
-    authorId: 2,
-    quote: "Ask for help, even if you break the companies website.",
-    hashTags: ["Main Instructor", "Dr.", "Zen", "Chemistry", "Balloon Gas Transport", "Forest"],
-    isAuthorAlive: true
-  }
-]
+const quotes = []
 
 export const getQuotes = () => {
   // .map is an array method - quote is one object in the quotes array
@@ -53,4 +12,22 @@ export const getAuthorIds = () => {
     return quote.authorId
   })
   return authorIds
+}
+
+//Returns a new unique id for the quote
+const getNewQuoteId = () => {
+  let highestOrderId = 0
+  if(quotes.length > 0){
+    highestOrderId = quotes.sort((a, b) => b.id - a.id)[0].id
+  }
+  return highestOrderId + 1
+}
+
+ // use a function with array method push
+export const addNewQuote = (newQuote) => {
+  const newId = getNewQuoteId()
+  newQuote.id = newId
+  quotes.push(newQuote)
+
+  document.dispatchEvent(new CustomEvent("stateChanged"))
 }
